fd7b13c89e4218c38578e57f18fd8f603085eff4
medha-14 <medha140904@gmail.com>
2024-08-21 23:11:09 +0530

    added text element

--- index.py
+++ index.py
@@ -6,7 +6,11 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock =pygame.time.Clock()
 sky=pygame.image.load('graphics/Sky.png')
+font=pygame.font.Font('font\Pixeltype.ttf',50)
 ground=pygame.image.load('graphics/ground.png')
+
+text=font.render('MY GAME',False,'Pink')
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -14,6 +18,7 @@ while True:
             exit()
     screen.blit(sky,(0,0))
     screen.blit(ground,(0,300))
+    screen.blit(text,(300,50))
     pygame.display.update()
     clock.tick(60)
 
