c6d1f0f9540c71eab10a4681e360b8f458f021fd
medha-14 <medha140904@gmail.com>
2024-08-21 23:29:31 +0530

    added moving snail

--- index.py
+++ index.py
@@ -9,7 +9,9 @@ sky=pygame.image.load('graphics/Sky.png')
 font=pygame.font.Font('font\Pixeltype.ttf',50)
 ground=pygame.image.load('graphics/ground.png')
 
-text=font.render('MY GAME',False,'Pink')
+text=font.render('RatRace',False,'Pink')
+snail=pygame.image.load('graphics\snail\snail1.png')
+snail_x=600
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +21,11 @@ while True:
     screen.blit(sky,(0,0))
     screen.blit(ground,(0,300))
     screen.blit(text,(300,50))
+    snail_x-=5
+    if snail_x<-100:
+         snail_x=800
+    screen.blit(snail,(snail_x,260))
+
     pygame.display.update()
     clock.tick(60)
 
