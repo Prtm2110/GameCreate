a621ff9b0f9c930e8e058ad9852e912c920ec366
medha-14 <medha140904@gmail.com>
2024-08-21 23:01:24 +0530

    blit screen

--- index.py
+++ index.py
@@ -5,16 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock =pygame.time.Clock()
-sky=pygame.image.load('garphics/Sky.png')
-ground=pygame.image.load('garphics/ground.png')
-
+sky=pygame.image.load('graphics/Sky.png')
+ground=pygame.image.load('graphics/ground.png')
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
-    # screen.blit(sky,(0,0))
-    screen.blit(ground,(0,200))
+    screen.blit(sky,(0,0))
+    screen.blit(ground,(0,300))
     pygame.display.update()
     clock.tick(60)
 
