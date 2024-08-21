8cee00f9600e1fe14cafe856f9efe3ceafd3f2e4
medha-14 <medha140904@gmail.com>
2024-08-22 01:13:50 +0530

    drew with rectangles

--- index.py
+++ index.py
@@ -9,7 +9,8 @@ sky=pygame.image.load('graphics/Sky.png').convert()
 font=pygame.font.Font('font\Pixeltype.ttf',50)
 ground=pygame.image.load('graphics/ground.png').convert()
 
-text=font.render('RatRace',False,'Pink')
+text=font.render('RatRace',1,(64,64,64))
+text_rect=text.get_rect(center=(400,50))
 snail=pygame.image.load('graphics\snail\snail1.png').convert_alpha()
 snail_x=600
 snail_rect=snail.get_rect(midbottom=(snail_x,300))
@@ -25,17 +26,21 @@ while True:
     # mouse=pygame.mouse.get_pos()
     # if player_rect.collidepoint((mouse)):
     #     print(pygame.mouse.get_pressed()) 
-    if event.type==pygame.MOUSEMOTION:
-        mouse=event.pos
+    # if event.type==pygame.MOUSEMOTION:
+    #     mouse=event.pos
 
-    if player_rect.collidepoint(mouse):
-        print('collision')
+    # if player_rect.collidepoint(mouse):
+    #     print('collision')
 
     screen.blit(sky,(0,0))
     screen.blit(ground,(0,300))
-    screen.blit(text,(300,50))
+    pygame.draw.rect(screen,'#c0e8ec',text_rect)
+    pygame.draw.rect(screen,'#c0e8ec',text_rect,10)
+    screen.blit(text,text_rect)
+   
     player_rect.left+=1
     screen.blit(player,player_rect)
+   
     snail_rect.left-=5
     if snail_rect.left<-100:
          snail_rect.left=800
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ # if event.type==pygame.MOUSEMOTION:
+    #     mouse=event.pos
+
+    # if player_rect.collidepoint(mouse):
+    #     print('collision')
